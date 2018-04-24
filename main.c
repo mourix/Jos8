b90f966bb4cc59408f9eab83b3c2b7d626611454
Jos van Mourik <mourix@live.nl>
2018-04-24 16:31:17 +0200

    Cleanup main.c and fix timing

--- main.c
+++ main.c
@@ -8,18 +8,14 @@ Based on multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter
 
 // includes
 #include <stdio.h>
-#include <stdlib.h>
 #include "chip8.h"
 #include "SDL2/SDL.h"
 
-// defines
+// scaling of 64x32 CHIP-8 screen
 #define SCALE 10
 
-// SDL variables
-SDL_Renderer *renderer;
-
 // SDL snancode to CHIP-8 keycode conversion
-const int keytype[16] = 
+const int keyconvert[16] = 
 {
 	SDL_SCANCODE_X, SDL_SCANCODE_1, SDL_SCANCODE_2, SDL_SCANCODE_3, 
 	SDL_SCANCODE_Q, SDL_SCANCODE_W, SDL_SCANCODE_E, SDL_SCANCODE_A,
@@ -27,22 +23,7 @@ const int keytype[16] =
 	SDL_SCANCODE_4, SDL_SCANCODE_R, SDL_SCANCODE_F, SDL_SCANCODE_V
 };
 
-// DEBUG: put graphics on command line				
-void debug_gxf(void)
-{
-	for(int y = 0; y < 32; y++)
-	{
-		for(int x = 0; x < 64; x++)
-		{
-			if(screen[x][y]) printf("*");
-			else printf(" ");
-		}
-		printf("\n");
-	}
-	system("cls");
-}
-
-// render a full frame to SDL
+// render a full frame
 void render_frame(SDL_Renderer *renderer)
 {
 	for(int y = 0; y < 32; y++)
@@ -67,8 +48,7 @@ void render_frame(SDL_Renderer *renderer)
 // main emulator loop
 int main(int argc, char *argv[]) 
 {
-	// running state
-    unsigned char running = 0;
+    unsigned char running = 0; // running state
     
     // load ROM if file argument exists
 	if(argc == 2) 
@@ -81,38 +61,30 @@ int main(int argc, char *argv[])
 	}
 	else printf("Usage: Jos8 [romname]");
 	
-    // initialize SDL
-    SDL_Init(SDL_INIT_VIDEO);
-    
-    // initialize SDL window
+    // setup SDL
+    SDL_Init(SDL_INIT_VIDEO); 
 	SDL_Window *window = SDL_CreateWindow("Jos8", SDL_WINDOWPOS_UNDEFINED, 
 						 SDL_WINDOWPOS_UNDEFINED, 64*SCALE, 32*SCALE, SDL_WINDOW_OPENGL);
-
-    // initialize SDL renderer
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	
-	// scale SDL renderer
-	SDL_RenderSetScale(renderer, SCALE, SCALE);
-
-	// SDL scankey pointer
-	const Uint8 *state = SDL_GetKeyboardState(NULL);
-
-	// SDL even union
+	SDL_Renderer *renderer = SDL_CreateRenderer
+							(window, -1, SDL_RENDERER_ACCELERATED); 
+	SDL_RenderSetScale(renderer, SCALE, SCALE); // scale SDL renderer
+	const Uint8 *state = SDL_GetKeyboardState(NULL); // SDL scankey pointer
     SDL_Event event;
 
+	// run emulator
     while(running)
     {
-		// process quit event
+		// process SDL quit button
         while(SDL_PollEvent(&event)) if(event.type == SDL_QUIT) running = 0;
         
-        // read keys into array
-        for(int k = 0; k < 16; k++) key[k] = state[keytype[k]];
+        // process keyboard input
+        for(int k = 0; k < 16; k++) key[k] = state[keyconvert[k]];
         
         // run cpu cycle and draw if screen flag is set
 		if(chip8_cycle() == 1) render_frame(renderer);
 		
-		// 500hz
-		SDL_Delay(2); 
+		// get the loop close to 500hz
+		SDL_Delay(1); 
 	}
 
 	// release SDL
