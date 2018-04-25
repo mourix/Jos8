75a1aac69817164fc3586ea6b54689537073872b
Jos van Mourik <mourix@live.nl>
2018-04-25 20:51:27 +0200

    Add address to debug prints

--- chip8.c
+++ chip8.c
@@ -112,71 +112,71 @@ int chip8_cycle(void)
 			{
 				// 00E0 	Display 	disp_clear() 	Clears the screen.	
 				case 0x0000:
+					if(debug) printf("0x%X Opcode 0x%X: 00E0 disp_clear\n", pc, opcode);
 					memset(screen, 0, sizeof(screen[0][0])*64*32);
 					pc += 2;
 					drawflag = 1; // update screen
-					if(debug) printf("Opcode 0x%X: 00E0 disp_clear\n", opcode);
 					break;
 				
 				// 00EE 	Flow 	return; 	Returns from a subroutine.
 				case 0x000E:
+					if(debug) printf("0x%X Opcode 0x%X: 00EE return\n", pc, opcode);
 					sp--;
 					pc = stack[sp];
-					if(debug) printf("Opcode 0x%X: 00EE return\n", opcode);
 					break;
 					
 				default:
-					printf ("Unknown opcode: 0x%X\n", opcode);
+					printf ("0x%X Unknown opcode: 0x%X\n", pc, opcode);
 			}
 			break;
 		
 		// 1NNN 	Flow 	goto NNN; 	Jumps to address NNN.
 		case 0x1000:
+			if(debug) printf("0x%X Opcode 0x%X: 1NNN goto NNN\n", pc, opcode);
 			pc = opcode & 0x0FFF;
-			if(debug) printf("Opcode 0x%X: 1NNN goto NNN\n", opcode);
 			break;
 			
 		// 2NNN 	Flow 	*(0xNNN)() 	Calls subroutine at NNN.	
 		case 0x2000:
+			if(debug) printf("0x%X Opcode 0x%X: 2NNN Call subroutine NNN\n", pc, opcode);
 			stack[sp] = pc + 2;
 			sp++;
 			pc = opcode & 0x0FFF;
-			if(debug) printf("Opcode 0x%X: 2NNN Call subroutine NNN\n", opcode);
 			break;
 			
 		// 3XNN 	Cond 	if(Vx==NN) 	Skips the next instruction if VX equals NN. 	
 		case 0x3000:
+			if(debug) printf("0x%X Opcode 0x%X: 3XNN skip if(Vx==NN)\n", pc, opcode);
 			if(V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) pc += 4;
 			else pc += 2;
-			if(debug) printf("Opcode 0x%X: 3XNN skip if(Vx==NN)\n", opcode);
 			break;
 				
 		// 4XNN 	Cond 	if(Vx!=NN) 	Skips the next instruction if VX doesn't equal NN. 	
 		case 0x4000:
+			if(debug) printf("0x%X Opcode 0x%X: 4XNN skip if(Vx!=NN)\n", pc, opcode);
 			if(V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) pc += 4;
 			else pc += 2;		
-			if(debug) printf("Opcode 0x%X: 4XNN skip if(Vx!=NN)\n", opcode);
 			break;
 			
 		// 5XY0 	Cond 	if(Vx==Vy) 	Skips the next instruction if VX equals VY. 
 		case 0x5000:
+			if(debug) printf("0x%X Opcode 0x%X: 5XY0 skip if(Vx==Vy)\n", pc, opcode);
 			if(V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4]) pc += 4;
 			else pc += 2;
-			if(debug) printf("Opcode 0x%X: 5XY0 skip if(Vx==Vy)\n", opcode);
 			break;
 			
 		// 6XNN 	Const 	Vx = NN 	Sets VX to NN.	
 		case 0x6000:
+			if(debug) printf("0x%X Opcode 0x%X: 6XNN Vx = NN \n", pc, opcode);
 			V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
 			pc += 2;
-			if(debug) printf("Opcode 0x%X: 6XNN Vx = NN \n", opcode);
 			break;
 			
 		// 7XNN 	Const 	Vx += NN 	Adds NN to VX. (Carry flag is not changed)
 		case 0x7000:
-			V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF);
+			if(debug) printf("0x%X Opcode 0x%X: 7XNN Vx += NN\n", pc, opcode);
+			V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
 			pc += 2;
-			if(debug) printf("Opcode 0x%X: 7XNN Vx += NN\n", opcode);
 			break;
 			
 		case 0x8000:
@@ -184,55 +184,56 @@ int chip8_cycle(void)
 			{
 				// 8XY0 	Assign 	Vx=Vy 	Sets VX to the value of VY.
 				case 0x0000:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY0 Vx=Vy\n", pc, opcode);
 					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY0 Vx=Vy\n", opcode);
 					break;
 					
 				// 8XY1 	BitOp 	Vx=Vx|Vy 	Sets VX to VX or VY. (Bitwise OR operation)
 				case 0x0001:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY1 Vx|Vy\n", pc, opcode);
 					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY1 Vx|Vy\n", opcode);
 					break;
 					
 				// 8XY2 	BitOp 	Vx=Vx&Vy 	Sets VX to VX and VY. (Bitwise AND operation)
 				case 0x0002:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY2 Vx=Vx&Vy\n", pc, opcode);
 					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY2 Vx=Vx&Vy\n", opcode);
 					break;
 					
 				// 8XY3 	BitOp 	Vx=Vx^Vy 	Sets VX to VX xor VY.
 				case 0x0003:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY3 Vx=Vx^Vy\n", pc, opcode);
 					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY3 Vx=Vx^Vy\n", opcode);
 					break;
 
 				// 8XY4 	Math 	Vx += Vy 	Adds VY to VX. 
 				// VF is set to 1 when there's a carry, and to 0 when there isn't.
 				case 0x0004:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY4 Vx += Vy\n", pc, opcode);
 					if((V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) >> 4]) > 255) V[0xF] = 1;
 					else V[0xF] = 0;
 					V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY4 Vx += Vy\n", opcode);
 					break;
 					
 				// 8XY5 	Math 	Vx -= Vy 	VY is subtracted from VX. 
 				// VF is set to 0 when there's a borrow, and 1 when there isn't.
 				case 0x0005:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY5 Vx -= Vy\n", pc, opcode);
 					if(V[(opcode & 0x0F00) >> 8] < V[(opcode & 0x00F0) >> 4]) V[0xF] = 0;
 					else V[0xF] = 1;
 					V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY5 Vx -= Vy\n", opcode);
 					break;
 					
 				// 8XY6 	BitOp 	Vx=Vy=Vy>>1 	Shifts VY right by one and copies the result to VX. 
 				// VF is set to the value of the least significant bit of VY before the shift.
 				case 0x0006:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY6 Vx=Vy=Vy>>1\n", pc, opcode);
 					if(cowgod)
 					{
 						V[0xF] = V[(opcode & 0x0F00) >> 8] & 1;
@@ -245,22 +246,22 @@ int chip8_cycle(void)
 						V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
 					}
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY6 Vx=Vy=Vy>>1\n", opcode);
 					break;
 					
 				// 8XY7 	Math 	Vx=Vy-Vx 	Sets VX to VY minus VX. 
 				// VF is set to 0 when there's a borrow, and 1 when there isn't.
 				case 0x0007:
+					if(debug) printf("0x%X Opcode 0x%X: 8XY7 Vx=Vy-Vx\n", pc, opcode);
 					if(V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4]) V[0xF] = 0;
 					else V[0xF] = 1;
 					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XY7 Vx=Vy-Vx\n", opcode);
 					break;
 					
 				// 8XYE 	BitOp 	Vx=Vy=Vy<<1 	Shifts VY left by one and copies the result to VX. 
 				// VF is set to the value of the most significant bit of VY before the shift.
 				case 0x000E:
+					if(debug) printf("0x%X Opcode 0x%X: 8XYE Vx=Vy=Vy<<1\n", pc, opcode);
 					if(cowgod)
 					{
 						V[0xF]  = (V[(opcode & 0x0F00) >> 8] & 128) >> 7;
@@ -273,46 +274,46 @@ int chip8_cycle(void)
 						V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
 					}
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: 8XYE Vx=Vy=Vy<<1\n", opcode);
 					break;
 	
 				default:
-					printf ("Unknown opcode: 0x%X\n", opcode);
+					printf ("0x%X Unknown opcode: 0x%X\n", pc, opcode);
 			}
 			break;
 			
 		// 9XY0 	Cond 	if(Vx!=Vy) 	Skips the next instruction if VX doesn't equal VY. 
 		// (Usually the next instruction is a jump to skip a code block)
 		case 0x9000:
+			if(debug) printf("0x%X Opcode 0x%X: 9XY0 skip if(Vx!=Vy)\n", pc, opcode);
 			if(V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4]) pc += 4;
 			else pc += 2;
-			if(debug) printf("Opcode 0x%X: 9XY0 skip if(Vx!=Vy)\n", opcode);
 			break;
 			
 		// 	ANNN 	MEM 	I = NNN 	Sets I to the address NNN.
 		case 0xA000:
+			if(debug) printf("0x%X Opcode 0x%X: ANNN I = NNN\n", pc, opcode);
 			I = opcode & 0x0FFF;
 			pc += 2;
-			if(debug) printf("Opcode 0x%X: ANNN I = NNN\n", opcode);
 			break;
 			
 		// BNNN 	Flow 	PC=V0+NNN 	Jumps to the address NNN plus V0.
 		case 0xB000:
+			if(debug) printf("0x%X Opcode 0x%X: BNNN PC=V0+NNN\n", pc, opcode);
 			pc = (opcode & 0x0FFF) + V[0];
-			if(debug) printf("Opcode 0x%X: BNNN PC=V0+NNN\n", opcode);
 			break;
 			
 		// 	CXNN 	Rand 	Vx=rand()&NN 	
 		// Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
 		case 0xC000:
+			if(debug) printf("0x%X Opcode 0x%X: CXNN Vx=rand()&NN\n", pc, opcode);
 			V[(opcode & 0x0F00) >> 8] = (rand() % 256) & (opcode & 0x00FF);
 			pc += 2;
-			if(debug) printf("Opcode 0x%X: CXNN Vx=rand()&NN\n", opcode);
 			break;	
 			
 		// 	DXYN 	Disp 	draw(Vx,Vy,N) 	
 		// Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
 		case 0xD000:;
+			if(debug) printf("0x%X Opcode 0x%X: DXYN draw(Vx,Vy,N)\n", pc, opcode);
 			unsigned char xs = V[(opcode & 0x0F00) >> 8];
 			unsigned char ys = V[(opcode & 0x00F0) >> 4];
 			unsigned char n = opcode & 0x000F;
@@ -335,7 +336,6 @@ int chip8_cycle(void)
 			
 			pc += 2;
 			drawflag = 1; // update screen
-			if(debug) printf("Opcode 0x%X: DXYN draw(Vx,Vy,N)\n", opcode);
 			break;
 			
 		case 0xE000:
@@ -343,20 +343,20 @@ int chip8_cycle(void)
 			{
 				// EX9E 	KeyOp 	if(key()==Vx) 	Skips the next instruction if the key stored in VX is pressed. 
 				case 0x000E:
+					if(debug) printf("0x%X Opcode 0x%X: EX9E if(key()==Vx)\n", pc, opcode);
 					if(key[V[(opcode & 0x0F00) >> 8]]) pc += 4;
 					else pc += 2;					
-					if(debug) printf("Opcode 0x%X: EX9E if(key()==Vx)\n", opcode);
 					break;
 				
 				// EXA1 	KeyOp 	if(key()!=Vx) 	Skips the next instruction if the key stored in VX isn't pressed. 
 				case 0x0001:
+					if(debug) printf("0x%X Opcode 0x%X: EXA1 if(key()!=Vx)\n", pc, opcode);
 					if(!key[V[(opcode & 0x0F00) >> 8]]) pc += 4;
 					else pc += 2;
-					if(debug) printf("Opcode 0x%X: EXA1 if(key()!=Vx)\n", opcode);
 					break;
 				
 				default:
-				printf ("Unknown opcode: 0x%X\n", opcode);
+				printf ("0x%X Unknown opcode: 0x%X\n", pc, opcode);
 			}
 			break;
 			
@@ -365,14 +365,15 @@ int chip8_cycle(void)
 			{
 				// FX07 	Timer 	Vx = get_delay() 	Sets VX to the value of the delay timer.
 				case 0x0007:
+					if(debug) printf("0x%X Opcode 0x%X: FX07 Vx = get_delay()\n", pc, opcode);
 					V[(opcode & 0x0F00) >> 8] = delay_timer;
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX07 Vx = get_delay()\n", opcode);
 					break;
 					
 				// FX0A 	KeyOp 	Vx = get_key() 	A key press is awaited, and then stored in VX. 
 				// (Blocking Operation. All instruction halted until next key event)
 				case 0x000A:
+					if(debug) printf("0x%X Opcode 0x%X: FX0A\n", pc, opcode);
 					for(int i = 0; i < 16; i++) // read input if no key was pressed last time
 					{
 						if(key[i] && keyflag == 16)
@@ -386,77 +387,76 @@ int chip8_cycle(void)
 						keyflag = 16;
 						pc += 2;
 					}
-					if(debug) printf("Opcode 0x%X: FX0A\n", opcode);
 					break;	
 								
 				// FX15 	Timer 	delay_timer(Vx) 	Sets the delay timer to VX.
 				case 0x0015:
+					if(debug) printf("0x%X Opcode 0x%X: FX15 delay_timer(Vx)\n", pc, opcode);
 					delay_timer = V[(opcode & 0x0F00) >> 8];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX15 delay_timer(Vx)\n", opcode);
 					break;		
 							
 				// FX18 	Sound 	sound_timer(Vx) 	Sets the sound timer to VX.
 				case 0x0018:
+					if(debug) printf("0x%X Opcode 0x%X: FX18 sound_timer(Vx)\n", pc, opcode);
 					sound_timer = V[(opcode & 0x0F00) >> 8];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX18 sound_timer(Vx)\n", opcode);
 					break;
 					
 				// FX1E 	MEM 	I +=Vx 	Adds VX to I.[3]
 				case 0x001E:
+					if(debug) printf("0x%X Opcode 0x%X: FX1E I +=Vx\n", pc, opcode);
 					I += V[(opcode & 0x0F00) >> 8];
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX1E I +=Vx\n", opcode);
 					break;	
 								
 				// FX29 	MEM 	I=sprite_addr[Vx] 	Sets I to the location of the sprite for the character in VX. 
 				case 0x0029:
+					if(debug) printf("0x%X Opcode 0x%X: FX29 I=sprite_addr[Vx] \n", pc, opcode);
 					I = 0x50 + (V[(opcode & 0x0F00) >> 8] * 5);
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX29 I=sprite_addr[Vx] \n", opcode);
 					break;	
 					
 				// FX33 	BCD 	set_BCD(Vx); Stores the binary-coded decimal representation of VX
 				case 0x0033:
+					if(debug) printf("0x%X Opcode 0x%X: FX33 set_BCD(Vx);\n", pc, opcode);
 					memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
 					memory[I+1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10;
 					memory[I+2] = (V[(opcode & 0x0F00) >> 8] % 100) % 10;
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX33 set_BCD(Vx);\n", opcode);
 					break;
 					
 				// FX55 	MEM 	reg_dump(Vx,&I) 	Stores V0 to VX (including VX) in memory starting at address I. 
 				// I is increased by 1 for each value written.
 				case 0x0055:
+					if(debug) printf("0x%X Opcode 0x%X: FX55 reg_dump(Vx,&I)\n", pc, opcode);
 					for(int i = 0; i <= ((opcode & 0x0F00) >> 8); i++)
 					{
 						memory[I] = V[i];
 						I++;
 					}
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX55 reg_dump(Vx,&I)\n", opcode);
 					break;	
 								
 				// FX65 	MEM 	reg_load(Vx,&I) 	Fills V0 to VX (including VX) with values from memory starting at address I. 
 				// I is increased by 1 for each value written.
 				case 0x0065:
+					if(debug) printf("0x%X Opcode 0x%X: FX65 reg_load(Vx,&I)\n", pc, opcode);
 					for(int i = 0; i <= ((opcode & 0x0F00) >> 8); i++)
 					{
 						V[i] = memory[I];
 						I++;
 					}						
 					pc += 2;
-					if(debug) printf("Opcode 0x%X: FX65 reg_load(Vx,&I)\n", opcode);
 					break;	
 								
 				default:
-				printf ("Unknown opcode: 0x%X\n", opcode);
+				printf ("0x%X Unknown opcode: 0x%X\n", pc, opcode);
 			}
 			break;			
 
 		default:
-			printf ("Unknown opcode: 0x%X\n", opcode);
+			printf ("0x%X Unknown opcode: 0x%X\n", pc, opcode);
 	}
 
 	// update delay timer	
